0e0c71c93bc96201760df7ce8710de4478a11a5e
Guilherme Tavares <guilhermet.silva@viannasempre.com.br>
2023-02-08 16:30:31 -0300

    funcionando

--- script.js
+++ script.js
@@ -4,24 +4,46 @@ const bandeirausa = document.querySelector('.bandeiraUSA')
 const bandeirabrasil = document.querySelector('.bandeiraBRASIL')
 const infoChange = document.querySelector('.infoChange')
 const moedaConvertida = document.querySelector('.moedaConvertida')
+const listaBandeiras = document.querySelector('.line').querySelectorAll('img')
+const input = document.querySelector('.inputid')
+console.log(listaBandeiras)
+let moeda = 'real'
 
-async function realizarReq(){
- let response = await fetch('https://economia.awesomeapi.com.br/json/USD-BRL')
- let json = await response.json()
+    async function realizarReq(){
+        let response = await fetch('https://economia.awesomeapi.com.br/json/USD-BRL')
+        let json = await response.json()
+        
+        if(input.value == ''){
+            return
+        }
+        moedaConvertida.querySelector('h2').innerHTML = `${json[0].high}`      
+        if (moeda =='real'){
+            let conta = (Number(input.value))/json[0].high
+            moedaConvertida.querySelector('p').innerText =`Com ${input.value} Reais você tem ${conta.toFixed(2)} Dólares`
+            moedaConvertida.style.display = 'flex'
+        }else{
+            let conta = (Number(input.value))*json[0].high
+            moedaConvertida.querySelector('p').innerText =`Com ${input.value} Dólares você tem ${conta.toFixed(2)} Reais`
+            moedaConvertida.style.display = 'flex'
+        }
+    }
 
- moedaConvertida.style.display= 'flex'
-
-  }
   function changeMoney(){
+      moedaConvertida.style.display = 'none'
     if (bandeirabrasil.style.display != 'none'){
         bandeirabrasil.style.display = 'none'
         bandeirausa.style.display = 'block'
         infoChange.innerText ='De Dólar para Real'
-        criaDiv()
+        moeda ='dolar'
     }else{
         bandeirabrasil.style.display = 'block'
         bandeirausa.style.display = 'none'
         infoChange.innerText ='De Real para Dólar'
-        criaDiv()
+        moeda = 'real'
     }
+  }
+
+  function criaDiv(){
+      console.log(json)
+      moedaConvertida.querySelector('h2').innerText = `U$$ ${json[0].high}`
   }
