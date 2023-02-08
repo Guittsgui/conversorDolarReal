1425f23c88561f54175de21f4dd6a149888d7235
Guilherme Tavares <guilhermet.silva@viannasempre.com.br>
2023-02-08 16:00:29 -0300

    testando conversor de moedas

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 const btEnviar = document.querySelector('.btEnviar').addEventListener('click',realizarReq)
-
+const btChange = document.querySelector('.change').addEventListener('click',changeMoney)
+const bandeirausa = document.querySelector('.bandeiraUSA')
+const bandeirabrasil = document.querySelector('.bandeiraBRASIL')
+const infoChange = document.querySelector('.infoChange')
 
 async function realizarReq(){
-   
-
  let response = await fetch('https://economia.awesomeapi.com.br/json/USD-BRL')
  let json = await response.json()
- console.log(json)
- console.log(json[0].code)
 
+
+
+  }
+  function changeMoney(){
+    if (bandeirabrasil.style.display != 'none'){
+        bandeirabrasil.style.display = 'none'
+        bandeirausa.style.display = 'block'
+        infoChange.innerText ='De Dólar para Real'
+    }else{
+        bandeirabrasil.style.display = 'block'
+        bandeirausa.style.display = 'none'
+        infoChange.innerText ='De Real para Dólar'
+    }
   }
