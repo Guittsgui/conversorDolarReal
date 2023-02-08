e6de3bf7e377bcd2e5a800d5c9ff0f3b68e04b2b
Guilherme Tavares <guilhermet.silva@viannasempre.com.br>
2023-02-08 16:04:20 -0300

    testando

--- script.js
+++ script.js
@@ -3,12 +3,13 @@ const btChange = document.querySelector('.change').addEventListener('click',chan
 const bandeirausa = document.querySelector('.bandeiraUSA')
 const bandeirabrasil = document.querySelector('.bandeiraBRASIL')
 const infoChange = document.querySelector('.infoChange')
+const moedaConvertida = document.querySelector('.moedaConvertida')
 
 async function realizarReq(){
  let response = await fetch('https://economia.awesomeapi.com.br/json/USD-BRL')
  let json = await response.json()
 
-
+ moedaConvertida.style.display= 'flex'
 
   }
   function changeMoney(){
@@ -16,9 +17,11 @@ async function realizarReq(){
         bandeirabrasil.style.display = 'none'
         bandeirausa.style.display = 'block'
         infoChange.innerText ='De Dólar para Real'
+        criaDiv()
     }else{
         bandeirabrasil.style.display = 'block'
         bandeirausa.style.display = 'none'
         infoChange.innerText ='De Real para Dólar'
+        criaDiv()
     }
   }
